b45b6b60ad06972cf4541e48cbeeb25d1693c208
TY_Lee <118336957+Wxm-233@users.noreply.github.com>
2024-03-02 16:49:47 +0800

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,73 +1,144 @@
 // create web server
-// create a web server with the http module
-const http = require('http');
-const fs = require('fs');
-const path = require('path');
-const url = require('url');
-const queryString = require('querystring');
-const comments = require('./comments');
-const { get } = require('http');
-const { parse } = require('path');
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+var qs = require('querystring');
+var template = require('./lib/template.js');
+var path = require('path');
 
-// create a web server
-http.createServer((req, res) => {
-    // get the url
-    const urlObj = url.parse(req.url, true);
-    const pathname = urlObj.pathname;
-    // get the query string
-    const query = urlObj.query;
-
-    // get the method
-    const method = req.method;
-
-    // set the response header
-    res.setHeader('Content-Type', 'text/html;charset=utf-8');
-
-    // set the response
-    if (pathname === '/' || pathname === '/index.html') {
-        // read the file
-        fs.readFile(path.join(__dirname, 'index.html'), 'utf-8', (err, data) => {
-            if (err) {
-                return res.end('404 Not Found');
-            }
-            // read the comments
-            comments.get().then((data) => {
-                res.end(data);
-            });
+var app = http.createServer(function(request, response){
+    var _url = request.url;
+    var queryData = url.parse(_url, true).query;
+    var pathname = url.parse(_url, true).pathname;
+    var title = queryData.id;
+    if(pathname === '/'){
+      if(queryData.id === undefined){
+        fs.readdir('./data', function(error, filelist){
+          var title = 'Welcome';
+          var description = 'Hello, Node.js';
+          var list = template.list(filelist);
+          var html = template.HTML(title, list,
+            `<h2>${title}</h2>${description}`,
+            `<a href="/create">create</a>`
+          );
+          response.writeHead(200);
+          response.end(html);
         });
-    } else if (pathname === '/add' && method === 'GET') {
-        // get the query string
-        const query = urlObj.query;
-        // add the comment
-        comments.add(query).then((data) => {
-            // redirect
-            res.writeHead(301, {
-                Location: '/'
-            });
-            res.end();
+      } else {
+        fs.readdir('./data', function(error, filelist){
+          var filteredId = path.parse(queryData.id).base;
+          fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
+            var title = queryData.id;
+            var list = template.list(filelist);
+            var html = template.HTML(title, list,
+              `<h2>${title}</h2>${description}`,
+              ` <a href="/create">create</a>
+                <a href="/update?id=${title}">update</a>
+                <form action="delete_process" method="post">
+                  <input type="hidden" name="id" value="${title}">
+                  <input type="submit" value="delete">
+                </form>`
+            );
+            response.writeHead(200);
+            response.end(html);
+          });
         });
-    } else if (pathname === '/add' && method === 'POST') {
-        // post the data
-        let data = '';
-        // get the post data
-        req.on('data', (chunk) => {
-            data += chunk;
-        });
-        req.on('end', () => {
-            // parse the query string
-            const query = queryString.parse(data);
-            // add the comment
-            comments.add(query).then((data) => {
-                // redirect
-                res.writeHead(301, {
-                    Location: '/'
-                });
-                res.end();
-            });
-        });
-    } else {
-        res.end('404 Not Found');
+      }
+    } else if(pathname === '/create'){
+      fs.readdir('./data', function(error, filelist){
+        var title = 'WEB - create';
+        var list = template.list(filelist);
+        var html = template.HTML(title, list, `
+          <form action="/create_process" method="post">
+            <p><input type="text" name="title" placeholder="title"></p>
+            <p>
+              <textarea name="description" placeholder="description"></textarea>
+            </p>
+            <p>
+              <input type="submit">
+            </p>
+          </form>
+        `, '');
+        response.writeHead(200);
+        response.end(html);
+      });
+    } else if(pathname === '/create_process'){
+      var body = '';
+      request.on('data', function(data){
+          body = body + data;
+      });
+      request.on('end', function(){
+          var post = qs.parse(body);
+          var title = post.title;
+          var description = post.description;
+          fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+            response.writeHead(302, {Location: `/?id=${title}`});
+            response.end();
+          });
+      });
     }
-}).listen(3000, () => {
-    console.log('Server is running at http://localhost:3000');
-});
+    else if(pathname === '/update'){
+      fs.readdir('./data', function(error, filelist){
+        var filteredId = path.parse(queryData.id).base;
+        fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
+          var title = queryData.id;
+          var list = template.list(filelist);
+          var html = template.HTML(title, list,
+            `
+            <form action="/update_process" method="post">
+              <input type="hidden" name="id" value="${title}">
+              <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+              <p>
+                <textarea name="description" placeholder="description">${description}</textarea>
+              </p>
+              <p>
+                <input type="submit">
+              </p>
+            </form>
+            `,
+            `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+          );
+          response.writeHead(200);
+          response.end(html);
+        });
+        });
+    }
+    else if(pathname === '/update_process'){
+      var body = '';
+      request.on('data', function(data){
+          body = body + data;
+      });
+      request.on('end', function(){
+          var post = qs.parse(body);
+          var id = post.id;
+          var title = post.title;
+          var description = post.description;
+          fs.rename(`data/${id}`, `data/${title}`, function(error){
+            fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+              response.writeHead(302, {Location: `/?id=${title}`});
+              response.end();
+            });
+          });
+      });
+    }
+    else if(pathname === '/delete_process'){
+      var body = '';
+      request.on('data', function(data){
+          body = body + data;
+      });
+      request.on('end', function(){
+          var post = qs.parse(body);
+          var id = post.id;
+          var filteredId = path.parse(id).base;
+          fs.unlink(`data/${filteredId}`, function(error){
+            response.writeHead(302, {Location: `/`});
+            response.end();
+          })
+      });
+    }
+    else {
+      response.writeHead(404);
+      response.end('Not found');
+    }
+}
+);
